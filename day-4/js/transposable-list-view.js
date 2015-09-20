8c6912fc342bcb8d3d593a46b12e8144462d8de1
Frank Kelso <frnkkls@yahoo.com>
2015-09-20 12:04:21 -0500

    added transposer model + view

--- day-4/js/transposable-list-view.js
+++ day-4/js/transposable-list-view.js
@@ -1,7 +1,15 @@
+// <div class="box" style="background: blue;"></div>
+
 var TransposableListView = function( $el, tlist, options ){
   return Object.create({
     render: function(){
-      this.initEvents();
+     var htmlResult = '';
+
+     tlist.list.forEach( function( colorValue, index){
+     	htmlResult += '<div class="box" style="background:' +colorValue + ';"></div>'
+     });
+
+      $el.html(htmlResult);
       return this;
     }
 
